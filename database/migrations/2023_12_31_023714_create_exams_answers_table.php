1588a11f7d6ff8c07b6bdee06a05ce516ea395a5
desihamita <69859667+desihamita@users.noreply.github.com>
2023-12-31 09:44:15 +0700

    exams_attempt & exams_answers table created

---
+++ database/migrations/2023_12_31_023714_create_exams_answers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exams_answers', function (Blueprint $table) {
+            $table->id('id');
+            $table->bigInteger('attempt_id');
+            $table->bigInteger('question_id');
+            $table->bigInteger('answer_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exams_answers');
+    }
+};
