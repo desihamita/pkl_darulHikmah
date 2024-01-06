da2d0491f7f355f0808dc66c36f8242a5f81b1e3
desihamita <69859667+desihamita@users.noreply.github.com>
2024-01-06 10:16:25 +0700

    update view login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,26 +1,66 @@
-@extends('layout.layout-common')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Log in</title>
 
-@section('space-work')
-    <h1>Login</h1>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
+  <link rel="stylesheet" href="{{asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
 
-    @if ($errors->any())
-        @foreach ($errors->all() as $error)
-            <p style="color:red;">{{ $error }}</p>
-        @endforeach
-    @endif
-
-    @if (Session::has('error'))
-        <p style="color:red;">{{ Session::get('error') }}</p>
-    @endif
-
-    <form action={{ route('userLogin') }} method="post">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+        <a href="{{ url('/') }}" class="h1"><b>Darul</b>Hikmah</a>
+    </div>
+    <div class="card-body">
+      <p class="login-box-msg">Sign in to start your session</p>
+      <form action={{ route('userLogin') }} method="post">
         @csrf
-        <input type="email" name="email" placeholder="Enter email">
-        <input type="password" name="password" placeholder="Enter password">
-        <input type="submit" value="Login">
-    </form>
+        <div class="input-group mb-3">
+          <input type="email" name="email" class="form-control" placeholder="Email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" name="password" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                <p style="color:red;">{{ $error }}</p>
+            @endforeach
+        @endif
 
-    <a href="/forget-password">Forget Password</a>
-@endsection
+        @if (Session::has('error'))
+            <p style="color:red;">{{ Session::get('error') }}</p>
+        @endif
+        <div class="row">
+          <div class="col-12">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('plugins/bootstrap/js')}}/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('dist/js')}}/adminlte.min.js')}}"></script>
+</body>
+</html>
+
 
 
